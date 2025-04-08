15027d6554a4364ed7a95d77d2f808ab07831032
Akbhar <112480476+AkbharChowdhury@users.noreply.github.com>
2025-04-08 21:08:41 +0100

    a

--- main.py
+++ main.py
@@ -1,15 +1,12 @@
-import csv
-import numpy as np
-import pandas as pd
 from collections import Counter
-from matplotlib import pyplot as plt
 
+import pandas as pd
+from matplotlib import pyplot as plt
+import hvplot.pandas
 
 def main():
     plt.style.use("fivethirtyeight")
-
     data = pd.read_csv('data.csv')
-    ids = data['Responder_id']
     lang_responses = data['LanguagesWorkedWith']
 
     language_counter = Counter()
@@ -17,31 +14,33 @@ def main():
     for response in lang_responses:
         language_counter.update(response.split(';'))
 
-    languages = []
-    popularity = []
-    lang_data:  dict[str, int] = {item[0]: item[1] for item in language_counter.most_common(15)}
-
-    for item in language_counter.most_common(15):
-
-        languages.append(item[0])
-        popularity.append(item[1])
-
-    languages.reverse()
-    popularity.reverse()
-    res = dict(reversed(list(lang_data.items())))
-    plt.barh(res.keys(), res.values())
+    languages_data:  dict[str, int] = {item[0]: item[1] for item in language_counter.most_common(15)}
+    languages_data = dict(reversed(list(languages_data.items())))
+    plt.barh(languages_data.keys(), languages_data.values())
 
 
     plt.title("Most Popular Languages")
-    # plt.ylabel("Programming Languages")
+    plt.ylabel("Programming Languages")
     plt.xlabel("Number of People Who Use")
 
     plt.tight_layout()
 
     plt.show()
+def x():
+    # data = {'x': ['A', 'B', 'C'], 'y1': [10, 15, 12], 'y2': [8, 9, 11]}
+    # df = pd.DataFrame(data)
+    # df.hvplot.bar(x='x', y=['y1', 'y2'], stacked=True)
+    # hvplot.bar()
 
+
+    data = {'x': ['A', 'B', 'C'], 'y1': [10, 15, 12], 'y2': [8, 9, 11]}
+    df = pd.DataFrame(data)  # Create a grouped bar chart
+    df.hvplot.bar(x='x', y=['y1', 'y2'], stacked=False)
+    df.hv
 
 if __name__ == '__main__':
-    main()
+    x()
+    # main()
+
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/

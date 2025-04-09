583c48759aecbd919d68d818470984997852f6bf
Akbhar <112480476+AkbharChowdhury@users.noreply.github.com>
2025-04-09 19:03:38 +0100

    a

--- main.py
+++ main.py
@@ -1,23 +1,19 @@
 from collections import Counter
 
+import numpy as np
 import pandas as pd
 from matplotlib import pyplot as plt
-import hvplot.pandas
+# import hvplot.pandas
 
 def main():
     plt.style.use("fivethirtyeight")
     data = pd.read_csv('data.csv')
     lang_responses = data['LanguagesWorkedWith']
-
     language_counter = Counter()
-
-    for response in lang_responses:
-        language_counter.update(response.split(';'))
-
-    languages_data:  dict[str, int] = {item[0]: item[1] for item in language_counter.most_common(15)}
+    [language_counter.update(response.split(';')) for response in lang_responses]
+    languages_data: dict[str, int] = {item[0]: item[1] for item in language_counter.most_common(15)}
     languages_data = dict(reversed(list(languages_data.items())))
     plt.barh(languages_data.keys(), languages_data.values())
-
 
     plt.title("Most Popular Languages")
     plt.ylabel("Programming Languages")
@@ -26,21 +22,11 @@ def main():
     plt.tight_layout()
 
     plt.show()
-def x():
-    # data = {'x': ['A', 'B', 'C'], 'y1': [10, 15, 12], 'y2': [8, 9, 11]}
-    # df = pd.DataFrame(data)
-    # df.hvplot.bar(x='x', y=['y1', 'y2'], stacked=True)
-    # hvplot.bar()
-
-
-    data = {'x': ['A', 'B', 'C'], 'y1': [10, 15, 12], 'y2': [8, 9, 11]}
-    df = pd.DataFrame(data)  # Create a grouped bar chart
-    df.hvplot.bar(x='x', y=['y1', 'y2'], stacked=False)
-    df.hv
 
 if __name__ == '__main__':
-    x()
-    # main()
+    main()
 
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
+
+

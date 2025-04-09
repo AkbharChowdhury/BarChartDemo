73fde194b054462df2a0c3085b0e8466690a61db
Akbhar <112480476+AkbharChowdhury@users.noreply.github.com>
2025-04-09 19:42:25 +0100

    a

--- main.py
+++ main.py
@@ -1,32 +1,22 @@
 from collections import Counter
-
-import numpy as np
 import pandas as pd
 from matplotlib import pyplot as plt
-# import hvplot.pandas
-
+import numpy as np
+from itertools import chain
 def main():
     plt.style.use("fivethirtyeight")
     data = pd.read_csv('data.csv')
-    lang_responses = data['LanguagesWorkedWith']
-    language_counter = Counter()
-    [language_counter.update(response.split(';')) for response in lang_responses]
+    languages = data['LanguagesWorkedWith']
+    language_counter = Counter(list(chain.from_iterable(list((language.split(';') for language in languages)))))
     languages_data: dict[str, int] = {item[0]: item[1] for item in language_counter.most_common(15)}
     languages_data = dict(reversed(list(languages_data.items())))
-    plt.barh(languages_data.keys(), languages_data.values())
-
+    plt.barh(languages_data.keys(), languages_data.values(), align='center')
     plt.title("Most Popular Languages")
     plt.ylabel("Programming Languages")
     plt.xlabel("Number of People Who Use")
-
     plt.tight_layout()
-
     plt.show()
+
 
 if __name__ == '__main__':
     main()
-
-
-
-
-

3efb03106c91afaf9081cc7c01764876409f5d7e
Akbhar <112480476+AkbharChowdhury@users.noreply.github.com>
2025-04-09 20:23:38 +0100

    a
    a

--- a.py
+++ a.py
@@ -1,24 +1,55 @@
-import matplotlib.pyplot as plt
-import numpy as np
-# Fixing random state for reproducibility
-np.random.seed(19680801)
-
-# Example data
-people = ('Tom', 'Dick', 'Harry', 'Slim', 'Jim')
-y_pos = np.arange(len(people))
-performance = 3 + 10 * np.random.rand(len(people))
-error = np.random.rand(len(people))
-
-fig, ax = plt.subplots()
-
-hbars = ax.barh(y_pos, performance, xerr=error, align='center')
-ax.set_yticks(y_pos, labels=people)
-ax.invert_yaxis()  # labels read top-to-bottom
-ax.set_xlabel('Performance')
-ax.set_title('How fast do you want to go today?')
-
-# Label with specially formatted floats
-ax.bar_label(hbars, fmt='%.2f')
-ax.set_xlim(right=15)  # adjust xlim to fit labels
-
-plt.show()
+# # import matplotlib.pyplot as plt
+# # import numpy as np
+# # # Fixing random state for reproducibility
+# # np.random.seed(19680801)
+# #
+# # # Example data
+# # people = ('Tom', 'Dick', 'Harry', 'Slim', 'Jim')
+# # y_pos = np.arange(len(people))
+# # performance = 3 + 10 * np.random.rand(len(people))
+# # error = np.random.rand(len(people))
+# #
+# # fig, ax = plt.subplots()
+# #
+# # hbars = ax.barh(y_pos, performance, xerr=error, align='center')
+# # ax.set_yticks(y_pos, labels=people)
+# # ax.invert_yaxis()  # labels read top-to-bottom
+# # ax.set_xlabel('Performance')
+# # ax.set_title('How fast do you want to go today?')
+# #
+# # # Label with specially formatted floats
+# # ax.bar_label(hbars, fmt='%.2f')
+# # ax.set_xlim(right=15)  # adjust xlim to fit labels
+# #
+# # plt.show()
+#
+#
+#
+# from collections import Counter
+# from itertools import chain
+#
+# import numpy as np
+# import pandas as pd
+# from matplotlib import pyplot as plt
+#
+#
+# def main():
+#     plt.style.use("fivethirtyeight")
+#     data = pd.read_csv('data.csv')
+#     languages = data['LanguagesWorkedWith']
+#     # language_counter = Counter(list(chain.from_iterable(list((  language.split(';') for language in languages)))))
+#     language_counter = Counter(list(chain.from_iterable(list((  language.split(';') for language in languages)))))
+#     language_counter = Counter(list(chain.from_iterable( [ language.split(';') for language in languages    ]))))
+#
+#     languages_data: dict[str, int] = {item[0]: item[1] for item in language_counter.most_common(15)}
+#     languages_data = dict(reversed(list(languages_data.items())))
+#     plt.barh(languages_data.keys(), languages_data.values(), align='center')
+#     plt.title("Most Popular Languages")
+#     plt.ylabel("Programming Languages")
+#     plt.xlabel("Number of People Who Use")
+#     plt.tight_layout()
+#     plt.show()
+#
+#
+# if __name__ == '__main__':
+#     main()
--- main.py
+++ main.py
@@ -1,13 +1,17 @@
+import itertools
 from collections import Counter
+from itertools import chain
+
+import numpy as np
 import pandas as pd
 from matplotlib import pyplot as plt
-import numpy as np
-from itertools import chain
+
+
 def main():
     plt.style.use("fivethirtyeight")
     data = pd.read_csv('data.csv')
     languages = data['LanguagesWorkedWith']
-    language_counter = Counter(list(chain.from_iterable(list((language.split(';') for language in languages)))))
+    language_counter = Counter(list(chain.from_iterable([language.split(';') for language in languages])))
     languages_data: dict[str, int] = {item[0]: item[1] for item in language_counter.most_common(15)}
     languages_data = dict(reversed(list(languages_data.items())))
     plt.barh(languages_data.keys(), languages_data.values(), align='center')
@@ -20,3 +24,7 @@ def main():
 
 if __name__ == '__main__':
     main()
+
+
+
+
